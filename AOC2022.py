ccb1df13c8b121bd9964d06baaf4d31c00e9d2ec
chnnn <87366178+chnnn@users.noreply.github.com>
2022-12-03 13:32:35 +0800

    Update AOC2022.py

--- AOC2022.py
+++ AOC2022.py
@@ -36,3 +36,35 @@ with open('input.txt') as f:
     print(part1, part2) #15337 #11696
     
 #day 3 ------------------------------------------------------------------------------------------------------------------------------------
+
+with open('input.txt') as f: #part 1
+    total = 0
+    for line in f:
+        line = line.strip()
+        sack1, sack2 = sorted(line[0:len(line)//2]), sorted(line[len(line)//2:])
+        
+        o = ord([i for i in sack1 if i in sack2][0])
+        if o <123 and o >96: total += o-96
+        else: total += o-38
+             
+    print(total) #7997 
+
+with open('input.txt') as f: #part 2
+    l = f.readlines()
+    l = [l[i*3:i*3+3] for i in range(len(l)//3)]
+
+total = 0
+for i in l:
+    i = [sorted(x.strip()) for x in i]
+    o = ord([y for y in i[0] if y in i[1] and y in i[2]][0])
+    if o <123 and o >96: total += o-96
+    else: total += o-38
+            
+print(total) #2545
+
+#day 4 ------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
